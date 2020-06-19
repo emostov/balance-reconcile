1f578594756e11e2a4b45f2f25d0871d03a8cc4e
Zeke Mostov <z.mostov@gmail.com>
2020-06-18 20:41:24 -0700

    setup subscribe

--- src/examples/main.ts
+++ src/examples/main.ts
@@ -1,20 +1,19 @@
-import { singleBlock } from "./singleBlock";
-import { exampleUseOfSubscribeWithLogging } from "./subscribe";
+import { subscribe, writeNonZeroDiffToFile } from "./subscribe";
 
 /**
  * If this is your first time using this repo, checkout `singleBlock`. It is
  * a good starting and probably the quickest example to get up and running.
  */
 async function main(): Promise<void> {
-  // const sidecarUrl = "http://127.0.0.1:8080/";
-  const sidecarUrl = "http://127.0.0.1:6262/";
+  const sidecarUrl = "http://127.0.0.1:8080/";
+  // const sidecarUrl = "http://127.0.0.1:6262/";
   // const wsUrl = "wss://rpc.polkadot.io";
+
   const wsUrl = "wss://kusama-rpc.polkadot.io/";
 
-  // await exampleUseOfSubscribeWithLogging(sidecarUrl, wsUrl);
+  const logger = writeNonZeroDiffToFile("Jun18Sub.txt");
 
-  await singleBlock(79, sidecarUrl, wsUrl);
-  // await singleBlock(2773005, sidecarUrl, wsUrl);
+  await subscribe(sidecarUrl, wsUrl, logger);
 }
 
 main().catch(console.log);
