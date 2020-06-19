a6e3c0e3dc0d0fd7f9cb584de7838589a642a22e
Zeke Mostov <z.mostov@gmail.com>
2020-06-18 20:13:56 -0700

    account for repatriated reserve

--- src/examples/main.ts
+++ src/examples/main.ts
@@ -5,15 +5,12 @@ import { exampleUseOfSubscribeWithLogging } from "./subscribe";
  * If this is your first time using this repo, checkout `singleBlock`. It is
  * a good starting and probably the quickest example to get up and running.
  */
+const WS_URL = process.env.NODE_WS_URL || "wss://kusama-rpc.polkadot.io/";
+const SIDECAR_URL = process.env.SIDECAR_URL || "http://127.0.0.1:8080/";
+
 async function main(): Promise<void> {
-  // const sidecarUrl = "http://127.0.0.1:8080/";
-  const sidecarUrl = "http://127.0.0.1:6262/";
-  // const wsUrl = "wss://rpc.polkadot.io";
-  const wsUrl = "wss://kusama-rpc.polkadot.io/";
-
-  // await exampleUseOfSubscribeWithLogging(sidecarUrl, wsUrl);
-
-  await singleBlock(79, sidecarUrl, wsUrl);
+  // await exampleUseOfSubscribeWithLogging(SIDECAR_URL, WS_URL);
+  await singleBlock(291678, SIDECAR_URL, WS_URL);
   // await singleBlock(2773005, sidecarUrl, wsUrl);
 }
 
